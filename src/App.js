f8ab58889a2719391f4e1a5a4fa2320949c9848b
Igor Crispim Diniz <Igor@MacBook-Pro.local>
2017-11-19 19:14:22 -0300

    Alert toasters added
    Now changes to a book's shelf are notified by a toaster alert that informs the user about the update status

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, withRouter } from 'react-router-dom'
 import './App.css'
 import * as BooksAPI from './BooksAPI'
 import BooksGrid from './BooksGrid'
 import ExamplePage from './ExamplePage'
 import Search from './Search'
+import { connect } from 'react-redux';
+import {
+  addNotification as notify,
+  updateNotification,
+  removeNotifications
+} from 'reapop';
 
 class BooksApp extends React.Component {
   state = {
     books: []
   }
 
+  static _getSendingMessage(shelvesNames, shelfType){
+    if(shelfType === 'none'){
+      return 'Removing book from shelves...';
+    }
+    else {
+      return `Sending book to ${ shelvesNames[shelfType] }...`;
+    }
+  }
+
+  static _getSucessMessage(shelvesNames, shelfType){
+    if(shelfType === 'none'){
+      return 'Book removed.';
+    }
+    else {
+      return `Book saved to ${ shelvesNames[shelfType] }.`
+    }
+  }
+
   changeBookShelfHandler = (bookId, toShelf) => {
+    const { notify, updateNotification } = this.props;
+    let notif = notify({
+      title: 'Update status',
+      message: BooksApp._getSendingMessage(BooksGrid.shelfNamesByType, toShelf),
+      status: 'loading',
+      dismissible: false,
+      dismissAfter: 0
+    });
+
     BooksAPI.update({ id: bookId }, toShelf)
       .then( () => {
-        BooksAPI.getAll()
-          .then( (books) => {
-            this.setState({ books: books })
-          })
-          .catch( () => {
-            this.setState({ books: [] })
-          })
+        return BooksAPI.getAll();
+      })
+      .then( (books) => {
+        notif.status = 'success';
+        notif.message = BooksApp._getSucessMessage(BooksGrid.shelfNamesByType, toShelf);
+        notif.dismissible = true;
+        notif.dismissAfter = 3000;
+        updateNotification(notif);
+        this.setState({ books: books })
+      })
+      .catch( () => {
+        notif.status = 'error';
+        notif.message = 'Sorry! Something went wrong. Please, try again in a while.';
+        notif.dismissible = true;
+        notif.dismissAfter = 3000;
+        updateNotification(notif);
+        this.setState({ books: [] })
       });
   }
 
@@ -54,4 +97,8 @@ class BooksApp extends React.Component {
   }
 }
 
-export default BooksApp
+export default withRouter(connect(null, {
+  notify,
+  updateNotification,
+  removeNotifications
+})(BooksApp));
--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import NotificationsSystem, { reducer as notificationsReducer } from 'reapop';
+import wyboTheme from 'reapop-theme-wybo';
+import { createStore, compose, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+// store
+const createStoreWithMiddleware = compose(applyMiddleware(thunk))(createStore);
+const store = createStoreWithMiddleware(combineReducers({
+  notifications: notificationsReducer()
+}), {});
 
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
+  <BrowserRouter>
+    <Provider store={ store }>
+      <div>
+        <NotificationsSystem theme={ wyboTheme }/>
+          <App />
+      </div>
+    </Provider>
+  </BrowserRouter>
+  ,
   document.getElementById('root')
 )
